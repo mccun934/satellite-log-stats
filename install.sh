4ec8ec43dce8eff409b8e72705b9ccc30c1f11d6
Mike McCune <mmccune@redhat.com>
2019-02-21 09:40:32 -0800

    Add mkdir + sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
 
-cp etc/cron.d/satellite /etc/cron.d/satellite
-cp etc/logrotate.d/satellite-log-stats /etc/logrotate.d/satellite-log-stats
-cp usr/sbin/satellite-log-stats /usr/sbin/satellite-log-stats
-chmod +x /usr/sbin/satellite-log-stats
+sudo mkdir /var/log/satellite/
+sudo cp etc/cron.d/satellite /etc/cron.d/satellite
+sudo cp etc/logrotate.d/satellite-log-stats /etc/logrotate.d/satellite-log-stats
+sudo cp usr/sbin/satellite-log-stats /usr/sbin/satellite-log-stats
+sudo chmod +x /usr/sbin/satellite-log-stats
 
 echo ""
 echo "Installation complete, logs will update every 10 minutes in /var/log/satellite/"
